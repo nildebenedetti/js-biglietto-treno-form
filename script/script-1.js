0a9093f1d1a6a400169ef39349e29067a7f2eaa5
“Nilde <nilde.benedetti@gmail.com>
2026-04-03 17:24:04 +0200

    add: function per calcolo prezzo biglietto in scope global

--- script/script-1.js
+++ script/script-1.js
@@ -1,3 +1,28 @@
+//*================== MY FIUNCTIONS ==============================*//
+
+// mi scrivo la funzione per calcolare il prezzo
+// come parametri mi servono eta'e tratta in km
+function myTicketPriceCalculator(age, fare) {
+    const prezzoKm = 0.21;
+    const prezzoTratta = prezzoKm * fare;
+    let sconto = 0;
+
+    if (age < 18) {
+        sconto = (prezzoTratta) * 0.2;
+    }
+
+    else if (age >= 65) {
+        sconto = (prezzoTratta) * 0.4;
+    }
+
+    let prezzoBiglietto = prezzoTratta - sconto;
+    return prezzoBiglietto;
+
+
+}
+
+//==================================================================//
+
 // vado a pescare tutte le variabili che mi serve pescare nell'html
 
 const nomePax = document.querySelector('#nome-pax');
@@ -8,7 +33,7 @@ const btnForm = document.querySelector('.btn-bellissimo');
 
 //scrivere il prograsmma event handler
 
-function clickBtnFormHandler (event) {
+function clickBtnFormHandler(event) {
     event.preventDefault();
     // inserisco input con valore
     // con il trim, puliamo tutti i valori da spazi extra
@@ -17,33 +42,31 @@ function clickBtnFormHandler (event) {
     let inputValueTratta = trattaKm.value.trim();
 
     // check itegrity e coherence per nome
-    if ( inputValueNome === '' || !isNaN(inputValueNome) ) {
+    if (inputValueNome === '' || !isNaN(inputValueNome)) {
         alert('inserisci un Nome valido!');
     }
 
-    if ( inputValueNome.length <= 1) {
+    if (inputValueNome.length <= 1) {
         alert('il nome è troppo corto!');
     }
     // check integrity e coherence cognome
-       if ( inputValueCognome === '' || !isNaN(inputValueCognome) ) {
+    if (inputValueCognome === '' || !isNaN(inputValueCognome)) {
         alert('inserisci un cognome valido!');
     }
 
-    if ( inputValueCognome.length <= 1) {
+    if (inputValueCognome.length <= 1) {
         alert('il cognome è troppo corto!');
     }
     // check integrity e coherence km
-       if ( inputValueTratta === '' || isNaN(inputValueTratta) ) {
+    if (inputValueTratta === '' || isNaN(inputValueTratta)) {
         alert('inserisci un valore numerico!');
     }
 
+    return;
 
-    return ;
-
-    
-    
-    
 }
+
+
 
 
 // aggiungere event listener a bottone
@@ -51,14 +74,14 @@ btnForm.addEventListener('click', clickBtnFormHandler);
 
 // NB togli il default con paramentro event!
 // COSA FA IL PROGRAMMA?
-    // toglie default del form --ok
-    // check coerenza e integrita''
-        // SE dato non va bene, alert inserisci dato corretto
-        // SE tutti i dati ok ---->
-            // SCRIVI FUNZIONE PER CALCOLARE IL PREZZO
-            // stampa i dati dei campi value con innerHTML
-            // dentro la card che HAI PREPARATO, VERO????
-            // RIMUOVERE d-none a container ticket
+// toglie default del form --ok
+// check coerenza e integrita'' --ok
+// SE dato non va bene, alert inserisci dato corretto --ok
+// SE tutti i dati ok ---->
+// SCRIVI FUNZIONE PER CALCOLARE IL PREZZO
+// stampa i dati dei campi value con innerHTML
+// dentro la card che HAI PREPARATO, VERO????
+// RIMUOVERE d-none a container ticket
 
 
 
