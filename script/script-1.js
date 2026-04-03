6962bf5f4c758b284e8064b70d56826fc4c7a079
“Nilde <nilde.benedetti@gmail.com>
2026-04-03 17:10:28 +0200

    add: controllo coerenza e integrita' per nome pax in event handler script1

--- script/script-1.js
+++ script/script-1.js
@@ -5,14 +5,30 @@ const cognomePax = document.querySelector('#cognome-pax');
 const trattaKm = document.querySelector('#tratta-km');
 const etaAnni = document.querySelector('#eta-anni');
 const btnForm = document.querySelector('.btn-bellissimo');
-
-
-
+const inputValueNome = 0;
 
 //scrivere il prograsmma event handler
 
-function clickBtnFormHandler () {
-    console.log('ma mi senti???');
+function clickBtnFormHandler (event) {
+    event.preventDefault();
+    // inserisco input con valore
+    // con il trim, puliamo tutti i valori da spazi extra
+    let inputValueNome = nomePax.value.trim();
+    
+    // let inputValue = inputValue.trim();
+    
+    if ( inputValueNome === '' || !isNaN(inputValueNome) ) {
+        alert('inserisci un Nome valido!');
+    }
+
+    if ( inputValueNome.length <= 1) {
+        alert('il nome è troppo corto!');
+    }
+
+    return ;
+
+    
+    
     
 }
 
@@ -22,7 +38,7 @@ btnForm.addEventListener('click', clickBtnFormHandler);
 
 // NB togli il default con paramentro event!
 // COSA FA IL PROGRAMMA?
-    // toglie default del form
+    // toglie default del form --ok
     // check coerenza e integrita''
         // SE dato non va bene, alert inserisci dato corretto
         // SE tutti i dati ok ---->
