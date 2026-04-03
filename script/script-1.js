83623fa032aa995d985c8d0affb5b4aa184f8e56
“Nilde <nilde.benedetti@gmail.com>
2026-04-03 15:58:34 +0200

    add: variabili  rilevanti per esecuzione programma in script1.js

--- script/script-1.js
+++ script/script-1.js
@@ -1,4 +1,7 @@
 // vado a pescare tutte le variabili che mi serve pescare nell'html
 
 const nomePax = document.querySelector('#nome-pax');
-console.log(nomePax.value);
+const cognomePax = document.querySelector('#cognome-pax');
+const trattaKm = document.querySelector('#tratta-km');
+const etaAnni = document.querySelector('#eta-anni');
+
