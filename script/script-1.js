dba2fc4a996098f076459151eac4aa830a88c954
“Nilde <nilde.benedetti@gmail.com>
2026-04-03 16:07:16 +0200

    add: ragionamento per programma in script1.js

--- script/script-1.js
+++ script/script-1.js
@@ -5,3 +5,19 @@ const cognomePax = document.querySelector('#cognome-pax');
 const trattaKm = document.querySelector('#tratta-km');
 const etaAnni = document.querySelector('#eta-anni');
 
+// aggiungere event listener a bottone
+//scrivere il prograsmma event handler
+// NB togli il default con paramentro event!
+// COSA FA IL PROGRAMMA?
+    // toglie default del form
+    // check coerenza e integrita''
+        // SE dato non va bene, alert inserisci dato corretto
+        // SE tutti i dati ok ---->
+            // RIMUOVERE d-none a container ticket
+            // stampa i dati dei campi value con innerHTML
+            // dentro la card che HAI PREPARATO, VERO????
+
+
+
+
+
